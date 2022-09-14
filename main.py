f521b54054ccd56f8c78de0dcc36cff6b2a4c2a9
Aleksandr Samoilov <sparco@nxt.ru>
2022-09-14 15:02:37 +0500

    third task done

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+
 class Student:
     def __init__(self, name, surname, gender):
         self.name = name
@@ -16,6 +17,21 @@ class Student:
         else:
             return 'Ошибка'
 
+    def _average_grade_hw(self):
+        result = sum(*self.grades.values()) / len(*self.grades.values())
+        return result
+
+    def __str__(self):
+        res = (f'Имя: {self.name}\nФамилия: {self.surname}\nСредняя оценка за домашнее задание: {self._average_grade_hw()}\n'
+            f'Курсы в процессе изучения: {", ".join(self.courses_in_progress)}\n'
+            f'Завершенные курсы: {", ".join(self.finished_courses)}')
+        return res
+
+    def __lt__(self,other):
+        if not isinstance(other,Student):
+            print('Not a Student!')
+            return
+        return self._average_grade_hw() < other._average_grade_hw()
         
 class Mentor:
     def __init__(self, name, surname):
@@ -33,6 +49,10 @@ class Reviewer(Mentor):
                 student.grades[course] = [grade]
         else:
             return 'Ошибка'
+    def __str__(self):
+        res = f'Имя: {self.name}\nФамилия: {self.surname}'
+        return res
+    
     
 
 class Lecturer(Mentor):
@@ -40,10 +60,23 @@ class Lecturer(Mentor):
         super().__init__(name,surname)
         self.grades = {}
 
-
+    def _average_rating(self):
+        result = sum(*self.grades.values()) / len(*self.grades.values())
+        return result
+    
+    def __str__(self):
+      res = f'Имя: {self.name}\nФамилия: {self.surname}\nСредняя оценка за лекции: {self._average_rating()}'
+      return res  
+        
+    def __lt__(self,other):
+        if not isinstance(other,Lecturer):
+            print('Not a Lecturer!')
+            return
+        return self._average_rating() < other._average_rating()
 
 best_student = Student('Ruoy', 'Eman', 'your_gender')
-best_student.courses_in_progress += ['Python']
+best_student.courses_in_progress += ['Python','Git']
+best_student.finished_courses = ['Введение в программирование']
  
 cool_reviewer = Reviewer('Some', 'Buddy')
 cool_reviewer.courses_attached += ['Python']
@@ -62,5 +95,9 @@ best_student.rate_lecturer(cool_lecturer, 'Python', 10)
 
 
  
-print(cool_lecturer.grades)
-print(best_student.grades)
+# print(cool_lecturer.grades)
+# print(best_student.grades)
+
+# print(cool_reviewer)
+# print(cool_lecturer)
+# print(best_student)
